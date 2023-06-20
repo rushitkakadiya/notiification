5d12f357b942de8d4c2903fbdea585c2077881ef
RUSHIT <122293949+rushitkakadiya@users.noreply.github.com>
2023-06-20 16:21:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post("/api/data", async (req, res, next) => {
       "to":item,
       "notification":{
         "title":`Property Alert`,
-        "body":`New ${Category} is added near to you at ${Area}`
+        "body":`New ${Category} is added near to you at your City`
       }
     }
     
